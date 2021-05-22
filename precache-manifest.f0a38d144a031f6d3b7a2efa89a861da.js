a78ea73c0fb9790a8e058a3cd0de9cc62dfa0339
omkarsarde <omkarsarde1995@gmail.com>
2021-05-22 09:36:48 -0400

    Updates

---
+++ precache-manifest.f0a38d144a031f6d3b7a2efa89a861da.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae29b07066a6094e422b9fdb62db2679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13737f5e798ff2beff90",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "4aaf0d02d931d942e757",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "4185f35e0f62b5ae56f6",
+    "url": "/static/css/main.6ce14d51.chunk.css"
+  },
+  {
+    "revision": "cce0b08105d7759939ad",
+    "url": "/static/js/2.c47b3c64.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.c47b3c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13737f5e798ff2beff90",
+    "url": "/static/js/3.0b70c643.chunk.js"
+  },
+  {
+    "revision": "4aaf0d02d931d942e757",
+    "url": "/static/js/4.094f5773.chunk.js"
+  },
+  {
+    "revision": "4185f35e0f62b5ae56f6",
+    "url": "/static/js/main.1400f247.chunk.js"
+  },
+  {
+    "revision": "ebe499fae1f9936208b5",
+    "url": "/static/js/runtime-main.b88139d6.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1411f43571704f82a6b86e0b6f321b03",
+    "url": "/static/media/RitLogo.1411f435.webp"
+  },
+  {
+    "revision": "a3e3d39827b69281b99804d5122c0336",
+    "url": "/static/media/RitLogo.a3e3d398.png"
+  },
+  {
+    "revision": "fb577773bc395a40e0b578ceb75bc31b",
+    "url": "/static/media/U1.fb577773.webp"
+  },
+  {
+    "revision": "57144b2bd16a2b6f93c7800cb1f5edfe",
+    "url": "/static/media/U10.57144b2b.webp"
+  },
+  {
+    "revision": "b03de5955fc36f1e6e9006123791038c",
+    "url": "/static/media/U2.b03de595.webp"
+  },
+  {
+    "revision": "98fdedbedd5f776cf0c2eabfac1a1ad4",
+    "url": "/static/media/U3.98fdedbe.webp"
+  },
+  {
+    "revision": "a264acfc27ce0af65ebca29213a70508",
+    "url": "/static/media/U4.a264acfc.webp"
+  },
+  {
+    "revision": "0835758b9dd275fb211cd778ab8f5b31",
+    "url": "/static/media/U5.0835758b.webp"
+  },
+  {
+    "revision": "38ce7ebe35ccd70e0e37bc7c62c8d16c",
+    "url": "/static/media/U6.38ce7ebe.webp"
+  },
+  {
+    "revision": "1b6022b1857aa9e44d6821e05045f5c8",
+    "url": "/static/media/U7.1b6022b1.webp"
+  },
+  {
+    "revision": "257975dd765375d112484e710e35f43a",
+    "url": "/static/media/U8.257975dd.webp"
+  },
+  {
+    "revision": "d38f1090e3dca6b9717d131c426e4787",
+    "url": "/static/media/U9.d38f1090.webp"
+  },
+  {
+    "revision": "178d049faeec40b96ee90f35c0ee7ca3",
+    "url": "/static/media/Viit.178d049f.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "104abcb3b41c3144fa742d58f747572b",
+    "url": "/static/media/drdo.104abcb3.png"
+  },
+  {
+    "revision": "3d66c497b5ac2bf3a739c8a75f121633",
+    "url": "/static/media/h.3d66c497.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "86411418d0e0aab35faf751950dd234a",
+    "url": "/static/media/sppu1.86411418.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
